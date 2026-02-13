a27f587c2d4f38a4395d93eda9332fc25b2555e6
jjurzak <xdkuba6@gmail.com>
2026-02-14 00:07:55 +0100

    added regex for col labels and some checking for eval func

--- solution.py
+++ solution.py
@@ -1,12 +1,25 @@
 import pandas as pd
+import re
 
 
 fruit_sales = pd.DataFrame({"quantity": [10, 3], "price": [10, 1]}, index=["banana", "apple"])
 print(fruit_sales)
 
+
+
+
 def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataFrame:
+    
+    if not re.match(r'^[A-Za-z_]+$', new_column):
+        raise ValueError("Col can only contain letter and underscore")
+
     df = df.copy()
-    df[new_column] = df.eval(role)
+
+    try:
+        df[new_column] = df.eval(role)
+    except Exception:
+        return pd.DataFrame()
+
     return df
 
 
